d745dfa614d96b89e8b1eec1b7244c560fc26fe1
M14i <102026659+MerzoukeMansouri@users.noreply.github.com>
2024-11-27 13:16:21 +0000

    sum2 test

--- src/sum2/sum2.spec.ts
+++ src/sum2/sum2.spec.ts
@@ -1,5 +1,5 @@
 import { sum } from "./sum2";
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+test('find indexes to make the sum the target', () => {
+    expect(sum([1,2,3,4], 7)).toBe([3,4]);
 });
--- src/sum2/sum2.ts
+++ src/sum2/sum2.ts
@@ -1,1 +1,10 @@
-export const sum = (a: number, b: number) => a + b;
+export const sum = (a: number[], b: number) => {
+    a.forEach((value1, index) => {
+        a.forEach( (va))
+    })
+    return null;
+}
+
+/// ALGO: algo sur des tableau (trouver les index dans un tableau pour que la somme des index face une valeur target )
+
+/// utilisation d'axios
