04a92bdbaab6b786eabf979de2b20312ac5d0be4
Merzouke Mansouri <merzouke@m14i.com>
2024-11-28 10:13:37 +0100

    Refactor sum function to sum2 for two sum problem

--- src/sum2/sum2.spec.ts
+++ src/sum2/sum2.spec.ts
@@ -1,5 +1,5 @@
-import { sum } from "./sum2";
+import { sum2 } from "./sum2";
 
 test('find indexes to make the sum the target', () => {
-    expect(sum([3,1,2,4],7)).toEqual([3,0]);
+    expect(sum2([3, 1, 2, 4], 7).sort()).toEqual([0, 3].sort());
 });
--- src/sum2/sum2.ts
+++ src/sum2/sum2.ts
@@ -1,14 +1,23 @@
-export const sum = (a: number[], b: number) => {
-    let result;
-    a.forEach((value1, index1) => {
-        a.forEach( (value2, index2) => {
-            if(value1 + value2 === b && index1 != index2) result = [index1, index2]
-        })
-    })
-    return result;
-}
-
-/// ALGO: algo sur des tableau 
-// (trouver les index dans un tableau pour que la somme des index face une valeur target )
-
-/// utilisation d'axios
+/**
+ * THE TWO SUM PROBLEM
+ * 
+ * Function to find two indices in an array whose elements sum up to a given target.
+ * 
+ * This function takes an array of numbers and a target value as inputs and returns
+ * an array containing the indices of two elements whose sum equals the target value.
+ * 
+ * Example:
+ *   Input: array = [2, 7, 11, 15], target = 9
+ *   Output: [0, 1] (because array[0] + array[1] = 2 + 7 = 9)
+ * 
+ * Note: 
+ * - Assumes there is exactly one solution, and the same element cannot be used twice.
+ * - Returns an empty array if no such indices exist.
+ * 
+ * @param array - The input array of numbers.
+ * @param target - The target sum value.
+ * @returns An array of two indices whose elements sum up to the target.
+ */
+export const sum2 = (array: number[], target: number): number[] => {
+    return [];
+};
